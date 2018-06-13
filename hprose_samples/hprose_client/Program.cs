099e0ae51b54640fb4434e652177eb195a3e5dc4
iamvoxer <7407514@qq.com>
2018-06-13 15:02:45 +0800

    hprose sample

---
+++ hprose_samples/hprose_client/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Hprose.Client;
+using Hprose.Common;
+using Hprose.IO;
+
+namespace hprose_client
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HproseHttpClient client = new HproseHttpClient(" http://localhost:2012/");
+            client.KeepAlive = true;
+            Console.WriteLine(client.Invoke("Hello",new Object[]{"bbbb"}));
+            Console.WriteLine(client.Invoke("hello",new Object[]{"cccc"}));
+            Console.ReadLine();
+        }
+    }
+}
---
+++ hprose_samples/hprose_server/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using Hprose.IO;
+using Hprose.Server;
+
+namespace hprose_server
+{
+    class TestService
+    {
+        public string Hello(string name)
+        {
+            return "Hello " + name + "!";
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HproseHttpListenerServer server = new HproseHttpListenerServer("http://localhost:2012/");
+            TestService ts = new TestService();
+            server.Add("Hello", ts);
+            server.IsCrossDomainEnabled = true;
+            //server.CrossDomainXmlFile = "crossdomain.xml";
+            server.Start();
+            Console.WriteLine("Server started.");
+            Console.ReadLine();
+            Console.WriteLine("Server stopped.");
+        }
+    }
+}
